5c72fe05091dad1a00349e9b7601244a25601b6b
Sicotronicoo <76023220+Sicotronicoo@users.noreply.github.com>
2021-10-24 22:19:11 +0200

    parejas

--- Boletin 4/ejercicio 2/parejas.js
+++ Boletin 4/ejercicio 2/parejas.js
@@ -1,19 +1,27 @@
-
 document.addEventListener("DOMContentLoaded", function () {
-    var td = document.getElementsByTagName("td");    
+    var imagenes = [];
+    var introducir;
+    var marcador = 0;
+    var td = document.getElementsByTagName("td");
     for (let item of td) {
         item.addEventListener("click", function () {
-            this.innerHTML = '<img src="img/'+item.getAttribute("name")+'.png"/>';
-        })
+            this.innerHTML = '<img src="img/' + item.getAttribute("name") + '.png"/>';
+            introducir = imagenes.push(item.getAttribute("name"));
+            if (introducir %2 == 0){
+                if (imagenes[0] == imagenes[1]) {
+                    marcador++;
+                    document.getElementById("marcador").value = marcador;
+                    document.getElementById(imagenes[0]).id = imagenes[0] + '1';
+                    document.getElementById(imagenes[0]).style.backgroundColor = "green";
+                    document.getElementById(imagenes[1]+'1').style.backgroundColor = "green";
+                    imagenes = [];
+                }
+                else {
+                    this.innerHTML = '<img src="img/interrogacion.png"/>';                    
+                    imagenes = [];
+                }
+            }
+        }
+        )
     }
 });
-
-
-
-
-
-
-
-
-
-
