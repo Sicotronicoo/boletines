30e4fa4819b69d725a6273f5e31c9ea4fb8fcba7
Sicotronicoo <martinezholguinluis@gmail.com>
2021-10-25 21:27:13 +0200

    pruebas

--- Boletin 4/ejercicio 2/parejas.js
+++ Boletin 4/ejercicio 2/parejas.js
@@ -1,28 +1,46 @@
 document.addEventListener("DOMContentLoaded", function () {
     var imagenes = [];
+    var imagenesid = [];
     var introducir;
     var marcador = 0;
     var td = document.getElementsByTagName("td");
     for (let item of td) {
         item.addEventListener("click", function () {
-            this.innerHTML = '<img src="img/' + item.getAttribute("name") + '.png"/>';
-            introducir = imagenes.push(item.getAttribute("name"));
+            this.innerHTML = '<img src="img/' + this.getAttribute("name")+ '.png" />';
+            var prueba = this.getAttribute("name");
+            introducir = imagenes.push(this.getAttribute("name"));
+            introducir = imagenesid.push(item.getAttribute("id"));
             if (introducir %2 == 0){
                 if (imagenes[0] == imagenes[1]) {
                     marcador++;
                     document.getElementById("marcador").value = marcador;
-                    document.getElementById(imagenes[0]).id = imagenes[0] + '1';
-                    document.getElementById(imagenes[0]).style.backgroundColor = "green";
-                    document.getElementById(imagenes[1]+'1').style.backgroundColor = "green";
+                    //document.getElementById(imagenes[0]).id = imagenes[0] + '1';
+                    document.getElementById(imagenesid[0]).style.backgroundColor = "green";
+                    document.getElementById(imagenesid[1]).style.backgroundColor = "green";
                     imagenes = [];
-                    //
                 }
                 else {
-                    this.innerHTML = '<img src="img/interrogacion.png"/>';                    
+                    //sleep(2000);
+
+                    //this.innerHTML = '<img src="img/interrogacion.png"/>';
+                    var ultimaimagen = imagenesid.pop();
+                    var td = document.getElementById(ultimaimagen);
+                     td.innerHTML = '<img src="img/interrogacion.png"/>';                    var ultimaimagen = imagenesid.pop();
+                     var td = document.getElementById(ultimaimagen);
+                      td.innerHTML = '<img src="img/interrogacion.png"/>';
                     imagenes = [];
+                    imagenesid = [];
                 }
             }
         }
         )
     }
 });
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+     if ((new Date().getTime() - start) > milliseconds) {
+      break;
+     }
+    }
+   }
