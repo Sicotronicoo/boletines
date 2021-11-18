4022beecab7f0787080f391b816860bf8517f55b
Sicotronicoo <martinezholguinluis@gmail.com>
2021-11-18 17:38:19 +0100

    putas expresiones

--- Boletin 4/ejercicio 4/expresionesRegulares.js
+++ Boletin 4/ejercicio 4/expresionesRegulares.js
@@ -1,19 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
- 
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("formulario").addEventListener('submit', validarFormulario);
+
 });
-  
-  function validarFormulario(evento) {
-    evento.preventDefault();
-    var cocinero  = [];
-    var nombreCocinero  = document.getElementById("cocinero");
-    function insert ( ) {
-        cocinero.push(nombreCocinero.value);
-        alert(cocinero);
-       }
-    if(cocinero[0] == "i"){
-        alert("Correcto");
-    }else{
-        alert("error");
-    } 
+
+function validarFormulario(evento) {
+  evento.preventDefault();
+  var cocinero = [];
+  cocinero = document.getElementById('cocinero').value;
+  if (/^[A-Z]{2}\W[0-9]{4}$/.test(cocinero) ) {
+    document.getElementById("cocinero").style.border = "0.3em green solid";
+  } else {
+    alert("El nombre del cocinero debe estar formado por dos letras en mayúscula, un símbolo y cuatro dígitos")
+    document.getElementById("cocinero").style.border = "0.3em red solid";
   }
+var destinatario = [];
+  destinatario = document.getElementById('destinatario').value;
+  if (/^[A-Z]{2,3} ?[_] *[a-z]{3,20} ?[:] *[0-9]{4}$/.test(destinatario)) {
+    document.getElementById("destinatario").style.border = "0.3em green solid";
+  } else {
+    alert("El destinatario debe tener dos o tres letras mayúsculas correspondientes al estado, un guión bajo, el nombre de la ciudad en minúsculas, dos puntos, y el código de distrito de 4 digitos")
+    document.getElementById("destinatario").style.border = "0.3em red solid";
+  }
+ var gramos;
+  gramos = document.getElementById('gramos').value;
+  if(gramos >= 100 && gramos <=5000){
+    document.getElementById("gramos").style.border = "0.3em green solid";
+  }else{
+    alert("Los gramos deben estar comprendidos entre 100 y 5000");
+    document.getElementById("gramos").style.border = "0.3em red solid";
+  }
+  var composicion =[];
+  composicion = composicion[0] = gramos+'g' + document.getElementById("composicion").value;
+  alert(composicion);
+  if(composicion[4] >=){
+  }
+}
